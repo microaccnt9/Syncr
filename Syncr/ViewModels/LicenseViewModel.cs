9cb8414f302eb4d2569576d89f57655b2279161f
Tamas Hamor <hamuguy@gmail.com>
2017-11-30 20:41:03 +0100

    License and improved about section

---
+++ Syncr/ViewModels/LicenseViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Syncr.Helpers;
+using Windows.Storage;
+using Windows.ApplicationModel;
+
+namespace Syncr.ViewModels
+{
+    public class LicenseViewModel : BackgroundThreadObservable
+    {
+        public LicenseViewModel()
+        {
+            ReadLicense();
+        }
+
+        private string licenseText;
+
+        public string LicenseText
+        {
+            get { return licenseText; }
+            set { Set(ref licenseText, value); }
+        }
+
+        private async void ReadLicense()
+        {
+            var file = await Package.Current.InstalledLocation.GetFileAsync(@"Assets\LICENSE");
+            LicenseText = await FileIO.ReadTextAsync(file);
+        }
+    }
+}
---
+++ Syncr/Views/LicensePage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using Syncr.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+
+namespace Syncr.Views
+{
+    public sealed partial class LicensePage : Page
+    {
+        public LicenseViewModel ViewModel { get; } = new LicenseViewModel();
+
+        public LicensePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
